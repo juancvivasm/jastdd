904456768d7715d6ee78ab5f150b95ae8cc7a5e3
JC <juanc.vivasm@gmail.com>
2023-02-21 17:38:27 -0300

    Codigo inicial

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/** Use Axios to get data from restcountries api */
+import axios from 'axios';
+
+/** Use the free API https://restcountries.com/
+ * You will use the following endpoints
+ * https://restcountries.com/v2/name/${name} for countries by name
+ * https://restcountries.com/v2/regionalbloc/${regionalbloc} for region blocks
+ */
+
+/** Create getCountry Function here */
+
+
+/** Create a test for this getRegion function */
+async function getRegionCountries(regionalbloc: string) {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const countries = [];
+  for (let i = 0; i < data.length; i++) {
+    countries.push(data[i].name);
+  }
+  return countries;
+}
+
+/** Create getRegionCapitals function here */
+
+
+export default {
+  getCountry,
+  getRegionCountries,
+  getRegionCapitals
+};
